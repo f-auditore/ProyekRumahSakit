41c1e2cd997a419af36201b5729cd9d43d7e91bb
zulfa011 <25010410002@universitasbumigora.ac.id>
2026-05-24 11:58:36 +0800

    add[ZL]: tambah methods validasi class Individu

--- src/model/Dokter.java
+++ src/model/Dokter.java
@@ -2,11 +2,21 @@ package model;
 
 public class Dokter extends Individu {
     protected String noLisensi;
-    protected int biaya;
+    protected String riwayatPendidikan;
 
-    public Dokter(String id, String nama, int usia, String noTelp, String noLisensi, int biaya){
-        super(id, nama, usia, noTelp);
+    public Dokter(String nik, String namaLengkap, int usia, String tempatLahir, String tanggalLahir, char jenisKelamin, String noLisensi, String riwayatPendidikan){
+        super(nik, namaLengkap, usia, tempatLahir, tanggalLahir, jenisKelamin);
         this.noLisensi = noLisensi;
-        this.biaya = biaya;
+        this.riwayatPendidikan = riwayatPendidikan;
+    }
+
+    @Override
+    public void getPeran(){
+        System.out.println("Dokter");
+    }
+
+    @Override
+    public void tampilkanInfo(){
+        System.out.println("NIK\t: " + nik + "\nNama\t: " + namaLengkap + "\nUsia\t: " + usia + "\nTempat Lahir\t: " + tempatLahir + "\nTanggal Lahir\t" + tanggalLahir +  "\nJenis Kelamin\t: " + jenisKelamin + "\nRiwayat Pendidikan\t: " + riwayatPendidikan);
     }
 }
--- src/model/Individu.java
+++ src/model/Individu.java
@@ -1,15 +1,74 @@
 package model;
 
-public abstract class Individu {
-    protected String id;
-    protected String nama;
-    protected int usia;
-    protected String noTelp;
+// public abstract class Individu {
+//     protected String id;
+//     protected String nama;
+//     protected int usia;
+//     protected String noTelp;
 
-    public Individu(String id, String nama, int usia, String noTelp){
-        this.id = id;
-        this.nama = nama;
+//     public Individu(String id, String nama, int usia, String noTelp){
+//         this.id = id;
+//         this.nama = nama;
+//         this.usia = usia;
+//         this.noTelp = noTelp;
+//     }
+// }
+
+public abstract class Individu {
+    protected String nik;
+    protected String namaLengkap;
+    protected int usia;
+    protected String tempatLahir;
+    protected String tanggalLahir;
+    protected char jenisKelamin;
+
+    public Individu(String nik, String namaLengkap, int usia, String tempatLahir, String tanggalLahir, char jenisKelamin){
+        this.nik = nik;
+        this.namaLengkap = namaLengkap;
         this.usia = usia;
-        this.noTelp = noTelp;
+        this.tempatLahir = tempatLahir;
+        this.tanggalLahir = tanggalLahir;
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    abstract void getPeran();
+    abstract void tampilkanInfo();
+
+    public String getNik(){
+        return nik;
+    }
+
+    public String getNama(){
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap){
+        if (namaLengkap != null && !namaLengkap.trim().isEmpty()) {
+            this.namaLengkap = namaLengkap;
+        }
+    }
+
+    public void setNik(String nik){
+        if (nik.length() == 16 && nik.matches("\\d+")) { //
+            this.nik = nik;
+        }
+    }
+    public void setTempatLahir(String tempatLahir){
+        if (tempatLahir != null && !tempatLahir.trim().isEmpty()) {
+            this.tempatLahir = tempatLahir;
+        }
+    }
+    public void setTanggalLahir(String tanggalLahir){
+        if (tanggalLahir.matches("\\d{2}-\\d{2}-\\d{4}")) {
+            this.tanggalLahir = tanggalLahir;
+        }
+    }
+    public void setJenisKelamin(char jenisKelamin){
+        this.jenisKelamin = jenisKelamin;
+    }
+    public void setUsia(int usia){
+        if(usia > 0 && usia < 150){
+
+        }
     }
 }
