17b292dd488ccbb7ddafd9fdf81c4062bae76fa5
unknown <dirwatsanami@gmail.com>
2026-05-25 10:07:36 +0800

    edit [DR]: 3 atribut baru pada jadwal

--- src/model/DokterSpesialis.java
+++ src/model/DokterSpesialis.java
@@ -1,27 +1,34 @@
 package model;
 
-public class DokterSpesialis extends Dokter{
+public class DokterSpesialis extends Dokter {
     protected String spesialis;
     protected int pengalamanTahun;
     protected int jmlhPasien;
-    //ruangan, status
-    
-    public DokterSpesialis(String nik, String namaLengkap, int usia, String tempatLahir, String tanggalLahir, char jenisKelamin, String noTelp, String noLisensi, String spesialis, int pengalamanTahun, int jmlhPasien){
+    // ruangan, status
+
+    public DokterSpesialis(String nik, String namaLengkap, int usia, String tempatLahir, String tanggalLahir,
+            char jenisKelamin, String noTelp, String noLisensi, String spesialis, int pengalamanTahun, int jmlhPasien) {
         super(nik, namaLengkap, usia, tempatLahir, tanggalLahir, jenisKelamin, noTelp, noLisensi);
         this.spesialis = spesialis;
         this.pengalamanTahun = pengalamanTahun;
         this.jmlhPasien = jmlhPasien;
     }
 
+    public String getSpesialis() {
+        return spesialis;
+    }
+
     @Override
-    public String getPeran(){
+    public String getPeran() {
         return "Dokter Spesialis";
     }
 
     @Override
-    public void tampilkanInfo(){
-        System.out.println("NIK\t: " + nik + "\nNama\t: " + namaLengkap + "\nUsia\t: " + usia + "\nTempat Lahir\t: " + tempatLahir + "\nTanggal Lahir\t: " + tanggalLahir + "\nJenis Kelamin\t: " + jenisKelamin + "\nRiwayat Pendidikan\t: " + spesialis + "\nPengalaman\t: " + pengalamanTahun + "\nJumlah pasien\t: " + jmlhPasien);
+    public void tampilkanInfo() {
+        System.out.println("NIK\t: " + nik + "\nNama\t: " + namaLengkap + "\nUsia\t: " + usia + "\nTempat Lahir\t: "
+                + tempatLahir + "\nTanggal Lahir\t: " + tanggalLahir + "\nJenis Kelamin\t: " + jenisKelamin
+                + "\nRiwayat Pendidikan\t: " + spesialis + "\nPengalaman\t: " + pengalamanTahun + "\nJumlah pasien\t: "
+                + jmlhPasien);
     }
 
 }
-
--- src/model/Pasien.java
+++ src/model/Pasien.java
@@ -3,19 +3,26 @@ package model;
 import java.util.ArrayList;
 
 public class Pasien extends Individu {
-    
+
     protected String penyakit;
-    
-    public Pasien(String nik, String namaLengkap, int usia, String tempatLahir, String tanggalLahir, char jenisKelamin, String noTelp, String penyakit){
+
+    public Pasien(String nik, String namaLengkap, int usia, String tempatLahir, String tanggalLahir, char jenisKelamin,
+            String noTelp, String penyakit) {
         super(nik, namaLengkap, usia, tempatLahir, tanggalLahir, jenisKelamin, noTelp);
         this.penyakit = penyakit;
     }
-                                    //ArrayList dataPasien dipanggil dari Main
-    public void tambahPasien(ArrayList<Pasien> dataPasien, String nik, String namaLengkap, int usia, String tempatLahir, String tanggalLahir, char jenisKelamin, String noTelp, String penyakit) {
+
+    // ArrayList dataPasien dipanggil dari Main
+    public void tambahPasien(ArrayList<Pasien> dataPasien, String nik, String namaLengkap, int usia, String tempatLahir,
+            String tanggalLahir, char jenisKelamin, String noTelp, String penyakit) {
         dataPasien.add(new Pasien(nik, namaLengkap, usia, tempatLahir, tanggalLahir, jenisKelamin, noTelp, penyakit));
     }
 
-    public void setPenyakit(String newPenyakit){
+    public String getPenyakit() {
+        return penyakit;
+    }
+
+    public void setPenyakit(String newPenyakit) {
         System.out.println(this.penyakit = newPenyakit);
     }
 
@@ -23,21 +30,22 @@ public class Pasien extends Individu {
         for (int i = 0; i < dataPasien.size(); i++) {
             if (dataPasien.get(i).getNik().equals(nik)) {
                 dataPasien.remove(i);
-                break; //keluar dari loop setelah pasien ditemukan dan dihapus 
+                break; // keluar dari loop setelah pasien ditemukan dan dihapus
             }
         }
     }
 
     @Override
-    public String getPeran(){
+    public String getPeran() {
         return "Pasien";
 
     }
 
     @Override
-    public void tampilkanInfo(){
-        System.out.println("NIK\t\t\t: " + nik + "\nNama\t\t\t: " + namaLengkap + "\nUsia\t\t\t: " + usia + "\nTempat Lahir\t: " + tempatLahir + "\nTanggal Lahir\t" + tanggalLahir + "\nJenis Kelamin\t\t: " + jenisKelamin + "\nPenyakit\t\t: " + penyakit); 
+    public void tampilkanInfo() {
+        System.out.println("NIK\t\t\t: " + nik + "\nNama\t\t\t: " + namaLengkap + "\nUsia\t\t\t: " + usia
+                + "\nTempat Lahir\t: " + tempatLahir + "\nTanggal Lahir\t" + tanggalLahir + "\nJenis Kelamin\t\t: "
+                + jenisKelamin + "\nPenyakit\t\t: " + penyakit);
     }
-
 
 }
