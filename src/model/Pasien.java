4c9880f20a8fb03b8af31518dcd5cd43c12513cc
zulfa011 <25010410002@universitasbumigora.ac.id>
2026-05-27 14:47:44 +0800

    edit[ZL]: format tampilkanInfo() pasien

--- src/model/Pasien.java
+++ src/model/Pasien.java
@@ -60,7 +60,7 @@ public class Pasien extends Individu {
     @Override
     public void tampilkanInfo() {
         System.out.println("NIK\t\t\t: " + nik + "\nNama\t\t\t: " + namaLengkap + "\nUsia\t\t\t: " + usia
-                + "\nTempat Lahir\t: " + tempatLahir + "\nTanggal Lahir\t" + tanggalLahir + "\nJenis Kelamin\t\t: "
+                + "\nTempat Lahir\t\t: " + tempatLahir + "\nTanggal Lahir\t\t: " + tanggalLahir + "\nJenis Kelamin\t\t: "
                 + jenisKelamin + "\nPenyakit\t\t: " + penyakit);
     }
 
